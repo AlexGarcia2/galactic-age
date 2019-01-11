14e6a4bfe67b9d6c894a61d6b1dfec0e62d6f456
Alex Garcia <g.nightster11@gmail.com>
2019-01-11 09:52:14 -0800

    add psuedo methods for logic ecpected to be done and bare bones specs for methods

--- spec/galactic-spec.js
+++ spec/galactic-spec.js
@@ -2,10 +2,34 @@ import {GalacDays} from './../src/galactic'
 
   describe ('this application should give a person their age on other planets', function(){
 
-    it ('should give a person age in the years of mercury',function(){
+    it ('should find the age of a person in earth years',function(){
 
       expect(true).toEqual(true);
     });
 
+    it('should give the age of a person in mercury years', function(){
+
+      expect(true).toEqual(true);
+    });
+
+    it('should give the age of a person in venus years', function(){
+
+      expect(true).toEqual(true);
+    });
+
+    it('should give the age of a person in mars years', function(){
+
+      expect(true).toEqual(true);
+    });
+
+    it('should give the age of a person in jupiter years', function(){
+
+      expect(true).toEqual(true);
+    });
+
+    it('should give the life expectancy of a person on given planet', function(){
+
+      expect(true).toEqual(true);
+    })
 
   });
--- src/galactic.js
+++ src/galactic.js
@@ -1,14 +1,48 @@
 class GalacDays {
-  constructor() {
+  constructor(month,day,year) {
+
+    this.month = month;
+    this.day = day;
+    this.year = year;
+
+  }
+
+  ageFiner(){
+    // finds the age of a person in human years
+
+    return (false);
 
   }
 
   mercury(){
     //need to give the age of a earth year in mercury years
 
-    return(true)
+    return(false)
   }
 
+  venus(){
+    //need to give back age in years of venus from eath years
+
+    return(false);
+  }
+
+  mars(){
+    // //need to give back age in years of mars from eath years
+
+    return (false);
+  }
+
+  jupiter(){
+    ////need to give back age in years of jupiter from eath years
+
+    return (false);
+  }
+
+  lifeExpect(){
+    // give life expectancy of person give the age and what planet they are on. if they are past the life expectancy of the planet give them back their age.
+
+    return (false);
+  }
 
 }
 export {GalacDays};
